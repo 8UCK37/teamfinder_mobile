4249bc5c26da0025af6c93df517ebfe3fb81e91f
8UCK37 <104382050+8UCK37@users.noreply.github.com>
2023-07-13 22:09:22 +0530

    online-offline indicator now works in real-time on home page

--- lib/widgets/online_widget.dart
+++ lib/widgets/online_widget.dart
@@ -1,8 +1,11 @@
+import 'dart:async';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:teamfinder_mobile/pojos/user_pojo.dart';
 import 'package:http/http.dart' as http;
+import 'package:teamfinder_mobile/services/socket_service.dart';
 
 class OnlineWidget extends StatefulWidget {
   const OnlineWidget({super.key});
@@ -14,11 +17,40 @@ class OnlineWidget extends StatefulWidget {
 class _OnlineWidgetState extends State<OnlineWidget>
     with SingleTickerProviderStateMixin {
   late List<UserPojo>? friendList = [];
+  late Map<String, bool>? onlineMap;
+  final SocketService socketService = SocketService();
+  StreamSubscription<dynamic>? _socketSubscription;
   @override
   void initState() {
     super.initState();
-
+    socketService.setupSocketConnection();
     _getFriendList();
+    incNoti();
+  }
+
+  @override
+  void dispose() {
+    // Unsubscribe the listener to avoid memory leaks
+    _socketSubscription?.cancel();
+    super.dispose();
+  }
+
+  void incNoti() {
+    _socketSubscription = socketService.getIncomingNoti().listen((data) {
+      //DateTime now = DateTime.now();
+      debugPrint('Received noti from socket: $data');
+      if (data['notification'] == 'disc') {
+        debugPrint('${data['sender']} disconnected');
+        setState(() {
+          onlineMap![data['sender']] = false;
+        });
+      } else if (data['notification'] == 'online') {
+        debugPrint('${data['sender']} is now online');
+        setState(() {
+          onlineMap![data['sender']] = true;
+        });
+      }
+    });
   }
 
   void _getFriendList() async {
@@ -49,12 +81,15 @@ class _OnlineWidgetState extends State<OnlineWidget>
               return 0; // order remains the same
             }
           });
+          onlineMap = {
+            for (var obj in parsedFriendList) obj.id: obj.isConnected
+          };
+
           friendList =
               parsedFriendList; // Update the state variable with the parsed list
         });
         // Use the postList for further processing or display
         // ignore: unused_local_variable
-        
       } else {
         // Request failed
         //print('Failed to hit Express backend endpoint');
@@ -97,8 +132,8 @@ class _OnlineWidgetState extends State<OnlineWidget>
             ),
           ),
           if (friendList != null) // Add a null check here
-          for (UserPojo user in friendList!) // Add a null check here
-            friendBubble(user)
+            for (UserPojo user in friendList!) // Add a null check here
+              friendBubble(user)
         ],
       ),
     );
@@ -106,24 +141,24 @@ class _OnlineWidgetState extends State<OnlineWidget>
 
   Widget friendBubble(UserPojo user) {
     return Padding(
-      padding: const EdgeInsets.only(left:8.0),
+      padding: const EdgeInsets.only(left: 8.0),
       child: Stack(
-              children: <Widget>[
-                CircleAvatar(
-                  radius: 22.0,
-                  backgroundImage: NetworkImage(user.profilePicture),
-                ),
-                if(user.isConnected)
-                const Positioned(
-                  right: 1.0,
-                  bottom: 5.0,
-                  child: CircleAvatar(
-                    radius: 7.0,
-                    backgroundColor: Color.fromARGB(255, 30, 135, 33),
-                  ),
-                ),
-              ],
+        children: <Widget>[
+          CircleAvatar(
+            radius: 22.0,
+            backgroundImage: NetworkImage(user.profilePicture),
+          ),
+          if (onlineMap![user.id]!)
+            const Positioned(
+              right: 1.0,
+              bottom: 5.0,
+              child: CircleAvatar(
+                radius: 7.0,
+                backgroundColor: Color.fromARGB(255, 12, 173, 17),
+              ),
             ),
+        ],
+      ),
     );
   }
 }
