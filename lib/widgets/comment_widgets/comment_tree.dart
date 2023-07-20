b6bac67ceb2f4e262ee6125e71f19673268bd436
8UCK37 <104382050+8UCK37@users.noreply.github.com>
2023-07-20 13:30:42 +0530

    comment lines finally work

--- lib/widgets/comment_widgets/comment_tree.dart
+++ lib/widgets/comment_widgets/comment_tree.dart
@@ -10,18 +10,25 @@ class CommentObj extends StatelessWidget {
   Widget build(BuildContext context) {
     List<Widget> render = [];
     for (CommentPojo parentComment in tree) {
-      render.add(commentBox(parentComment, 18));
+      int depth = 0;
+      depth = depth + 1;
+      render.add(commentBox(parentComment, 18,
+          (parentComment.children!.length == 0), true, depth));
       if (parentComment.children != null) {
         for (CommentPojo child in parentComment.children!) {
+          depth = depth + 1;
           render.add(Padding(
-            padding: const EdgeInsets.only(left: 15),
-            child: commentBox(child, 15),
+            padding: const EdgeInsets.only(left: 25),
+            child: commentBox(
+                child, 15, (child.children!.length == 0), false, depth),
           ));
           if (child.children != null) {
             for (CommentPojo grandKid in child.children!) {
+              depth = depth + 1;
               render.add(Padding(
-                padding: const EdgeInsets.only(left: 30),
-                child: commentBox(grandKid, 15),
+                padding: const EdgeInsets.only(left: 47),
+                child: commentBox(grandKid, 15,
+                    (grandKid.children!.length == 0), false, depth),
               ));
             }
           }
@@ -37,7 +44,8 @@ class CommentObj extends StatelessWidget {
     ));
   }
 
-  Widget commentBox(CommentPojo comment, double radius) {
+  Widget commentBox(CommentPojo comment, double radius, bool isLast,
+      bool isFirst, int index) {
     return SafeArea(
       child: SizedBox(
         child: Row(
@@ -45,14 +53,30 @@ class CommentObj extends StatelessWidget {
           children: [
             Column(
               children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 8.0),
-                  child: CircleAvatar(
-                    radius: radius,
-                    backgroundImage:
-                        NetworkImage(comment.author!.profilePicture),
-                  ),
+                Row(
+                  children: [
+                    if (!isFirst)
+                      CustomPaint(
+                        painter: ArrowPainter(),
+                      ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 8.0),
+                      child: CircleAvatar(
+                        radius: radius,
+                        backgroundImage:
+                            NetworkImage(comment.author!.profilePicture),
+                      ),
+                    ),
+                  ],
                 ),
+                if (!isLast)
+                  CustomPaint(
+                    painter: LinePainter(comment),
+                  ),
+                if (isLast && !isFirst)
+                  CustomPaint(
+                    painter: UpwardLinePainter(index.toDouble()),
+                  ),
               ],
             ),
             Expanded(
@@ -106,5 +130,103 @@ class CommentObj extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+class LinePainter extends CustomPainter {
+  final CommentPojo comment;
+  LinePainter(this.comment);
+
+  double countOffspring(CommentPojo comment) {
+    int count = comment.children?.length ?? 0;
+    if (comment.children!.length == 1) {
+      return 1;
+    }
+    if (comment.children != null) {
+      for (CommentPojo child in comment.children!) {
+        count += countOffspring(child).toInt();
+      }
+    }
+    return count.toDouble();
+  }
+
+  @override
+  void paint(
+    Canvas canvas,
+    Size size,
+  ) {
+    final paint = Paint()
+      ..color = Colors.blue
+      ..strokeWidth = 2.0
+      ..style = PaintingStyle.stroke;
+
+    final path = Path();
+    path.moveTo(0, 0); // Starting point
+    path.lineTo(0, 63 * countOffspring(comment)); // First vertical line
+    //path.lineTo(14, 55); // Bottom horizontal line
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
+  }
+}
+
+class ArrowPainter extends CustomPainter {
+  @override
+  void paint(
+    Canvas canvas,
+    Size size,
+  ) {
+    final paint = Paint()
+      ..color = Colors.blue
+      ..strokeWidth = 2.0
+      ..style = PaintingStyle.stroke;
+
+    final path = Path();
+    path.moveTo(0, 5); // Starting point
+    path.lineTo(-8, 5); // horizontal line
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
+  }
+}
+
+class UpwardLinePainter extends CustomPainter {
+  final double multiplier;
+  UpwardLinePainter(this.multiplier);
+  double formatMultiplier() {
+    debugPrint(this.multiplier.toString());
+    if (this.multiplier == 1) {
+      return this.multiplier * 0.5;
+    } else {
+      return this.multiplier;
+    }
+  }
+
+  @override
+  void paint(
+    Canvas canvas,
+    Size size,
+  ) {
+    final paint = Paint()
+      ..color = Colors.blue
+      ..strokeWidth = 2.0
+      ..style = PaintingStyle.stroke;
+
+    final path = Path();
+    path.moveTo(-22, -15); // Starting point
+    path.lineTo(-22, -22.5 * formatMultiplier()); // vertical line
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
   }
 }
