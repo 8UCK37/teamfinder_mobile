ed2f38af12e2e4f460faa749188f582472bd7576
8UCK37 <104382050+8UCK37@users.noreply.github.com>
2023-12-24 22:50:01 +0530

    made some changes to the login screen

--- lib/pages/login_screen.dart
+++ lib/pages/login_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:sign_button/sign_button.dart';
 import 'package:teamfinder_mobile/services/auth_service.dart';
@@ -12,46 +14,44 @@ class LoginActivity extends StatefulWidget {
 class _LoginActivityState extends State<LoginActivity> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Container(
-        height: double.infinity,
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [Color(0xFF4B39EF), Color(0xFFEE8B60)],
-            stops: [0, 1],
-            begin: AlignmentDirectional(0.87, -1),
-            end: AlignmentDirectional(-0.87, 1),
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Container(
+          height: double.infinity,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Color.fromARGB(255, 218, 74, 69), Color.fromARGB(255, 80, 19, 11)],
+              stops: [0, 1],
+              begin: AlignmentDirectional(0.87, -1),
+              end: AlignmentDirectional(-0.87, 1),
+            ),
           ),
-        ),
-        alignment: const AlignmentDirectional(0, -1),
-        child: SingleChildScrollView(
+          alignment: const AlignmentDirectional(0, -1),
           child: Column(
             mainAxisSize: MainAxisSize.max,
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Padding(
-                padding: const EdgeInsetsDirectional.fromSTEB(0, 70, 0, 32),
-                child: Container(
-                  width: 200,
-                  height: 70,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(16),
-                  ),
-                  alignment: AlignmentDirectional(0, 0),
-                  child: const Text('TeamFinder',
-                      style: TextStyle(
-                        fontFamily: 'Plus Jakarta Sans',
-                        color: Colors.white,
-                        fontSize: 15,
-                        fontWeight: FontWeight.w600,
-                      )),
+              Container(
+                width: 200,
+                height: 70,
+                decoration: BoxDecoration(
+                  //border:Border.all(color: Colors.blue),
+                  borderRadius: BorderRadius.circular(16),
                 ),
+                alignment: const AlignmentDirectional(0, 0),
+                child: const Text('CallOut',
+                    style: TextStyle(
+                      fontFamily: 'Caveat',
+                      color: Colors.white,
+                      fontSize: 45,
+                      fontWeight: FontWeight.w600,
+                    )),
               ),
               ClipRRect(
                 borderRadius: BorderRadius.circular(250),
                 child: Image.asset(
-                  'assets/images/logo-no-background.png',
+                  'assets/images/megaphone.png',
                   width: MediaQuery.sizeOf(context).width * 0.5,
                   height: 200,
                   fit: BoxFit.fill,
