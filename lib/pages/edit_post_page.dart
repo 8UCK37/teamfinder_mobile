10f01567e385f1367f509ea94d164aedcf03ae57
8UCK37 <104382050+8UCK37@users.noreply.github.com>
2024-01-06 13:32:55 +0530

    post editing works i.e description and mention list (not sure if letting users change images was the plan or not)

--- lib/pages/edit_post_page.dart
+++ lib/pages/edit_post_page.dart
@@ -1,9 +1,17 @@
+import 'dart:convert';
+
 import 'package:carousel_images/carousel_images.dart';
+import 'package:dio/dio.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
+import 'package:quickalert/models/quickalert_type.dart';
+import 'package:quickalert/widgets/quickalert_dialog.dart';
 import 'package:simply_mentions/text/mention_text_editing_controller.dart';
 import 'package:teamfinder_mobile/pojos/post_pojo.dart';
 import 'package:teamfinder_mobile/services/data_service.dart';
+import 'package:teamfinder_mobile/services/mention_service.dart';
 import 'package:teamfinder_mobile/services/notification_observer.dart';
 import 'package:teamfinder_mobile/widgets/misc/image_viewer.dart';
 import 'package:teamfinder_mobile/widgets/misc/teamfinder_appbar.dart';
@@ -29,6 +37,47 @@ class _EditPostState extends State<EditPost> {
     convertIntoMarkUp();
   }
 
+  void editPost(List<dynamic> opsList) async {
+    Dio dio = Dio();
+    final user = FirebaseAuth.instance.currentUser;
+    final idToken = await user!.getIdToken();
+    Options options = Options(
+      headers: {
+        'Authorization': 'Bearer $idToken',
+        'contentType': 'multipart/form-data'
+      },
+    );
+    try {
+      FormData formData = FormData();
+
+      ///IMPORTANT have to  do this map thingy before we foreach append each image file
+      ///or this will overwrite every file added
+      formData = FormData.fromMap({
+        'data': jsonEncode({
+          'id': widget.post.id,
+          'data': [],
+          'desc': {
+            'content': {'ops': opsList}
+          }
+        }),
+      });
+
+      Response response = await dio.post(
+        'http://${dotenv.env['server_url']}/editPost',
+        data: formData,
+        options: options,
+      );
+      if (response.statusCode == 200) {
+        debugPrint("post uploaded with: ${response.statusCode.toString()}");
+        // ignore: use_build_context_synchronously
+      } else {
+        debugPrint('failed with: ${response.statusCode.toString()}');
+      }
+    } catch (error) {
+      debugPrint('Error from create_post_page line 91: $error');
+    }
+  }
+
   String convertIntoMarkUp() {
     if (widget.post.description == null) {
       return '';
@@ -49,10 +98,12 @@ class _EditPostState extends State<EditPost> {
   Widget build(BuildContext context) {
     final userService = Provider.of<ProviderService>(context, listen: true);
     final notiObserver = Provider.of<NotificationWizard>(context, listen: true);
+    final mentionService = Provider.of<MentionService>(context, listen: true);
     final List<String> imageList = (widget.post.shared == null)
         ? widget.post.photoUrl!.split(',')
         : widget.post.parentpost!.photoUrl!.split(',');
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: TeamFinderAppBar(
         titleText: "Edit Post",
         isDark: userService.darkTheme!,
@@ -62,7 +113,18 @@ class _EditPostState extends State<EditPost> {
       ),
       floatingActionButton: GestureDetector(
         onTap: () {
-
+          editPost(mentionService.deltaParser());
+          QuickAlert.show(
+            context: context,
+            type: QuickAlertType.success,
+            text: 'Post has been edited!!',
+            onConfirmBtnTap: () {
+              Future.delayed(const Duration(milliseconds: 500), () {
+                userService.getOwnPost();
+                Navigator.of(context).pop();
+              });
+            },
+          );
         },
         child: const Material(
           elevation: 20,
