9767caa7c8f368ebbe21f7200ca7acd0772b37f1
8UCK37 <104382050+8UCK37@users.noreply.github.com>
2024-01-06 22:14:10 +0530

    privacy setting can now be changed from settings page

--- lib/pages/menu_pages/settings.dart
+++ lib/pages/menu_pages/settings.dart
@@ -1,7 +1,12 @@
+import 'package:animated_custom_dropdown/custom_dropdown.dart';
 import 'package:day_night_switcher/day_night_switcher.dart';
+import 'package:dio/dio.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:teamfinder_mobile/controller/network_controller.dart';
 import 'package:teamfinder_mobile/services/data_service.dart';
 import '../../widgets/misc/teamfinder_appbar.dart';
 
@@ -17,10 +22,79 @@ class SettingsPage extends StatefulWidget {
 class _SettingsPageState extends State<SettingsPage> {
   late SharedPreferences _preferences;
   late bool _isDark = widget.isDarkCurrent;
+
+  String? currentFriendListPrivacy;
+  String? currentLinkedPrivacy;
+
+  final Map<String, Color> privacyOptions = {
+    'Public': Colors.green,
+    'OnlyFriends': Colors.blue,
+    'Private': Colors.red,
+  };
+
   @override
   void initState() {
-    super.initState();
+    userInfoInit();
     _initializePreferences();
+    super.initState();
+  }
+
+  void updateFriendListVis() async {
+    int id = privacyOptions.keys.toList().indexOf(currentFriendListPrivacy!);
+    NetworkController networkController = NetworkController();
+    if (await networkController.noInternet()) {
+      debugPrint("updateFriendListVis() no_internet");
+      return;
+    } else {
+      debugPrint("updateFriendListVis() called");
+    }
+
+    Dio dio = Dio();
+    final user = FirebaseAuth.instance.currentUser;
+
+    final idToken = await user!.getIdToken();
+    Options options = Options(
+      headers: {
+        'Authorization': 'Bearer $idToken',
+      },
+    );
+    var response = await dio.post(
+      'http://${dotenv.env['server_url']}/updateFriendListVisPref',
+      data: {'pref': id},
+      options: options,
+    );
+    if (response.statusCode == 200) {
+      debugPrint("");
+    }
+  }
+
+  void updateLinkedAccVis() async {
+    int id = privacyOptions.keys.toList().indexOf(currentLinkedPrivacy!);
+    NetworkController networkController = NetworkController();
+    if (await networkController.noInternet()) {
+      debugPrint("updateLinkedAccVis() no_internet");
+      return;
+    } else {
+      debugPrint("updateLinkedAccVis() called");
+    }
+
+    Dio dio = Dio();
+    final user = FirebaseAuth.instance.currentUser;
+
+    final idToken = await user!.getIdToken();
+    Options options = Options(
+      headers: {
+        'Authorization': 'Bearer $idToken',
+      },
+    );
+    var response = await dio.post(
+      'http://${dotenv.env['server_url']}/updateLinkedVisPref',
+      data: {'pref': id},
+      options: options,
+    );
+    if (response.statusCode == 200) {
+      debugPrint("");
+    }
   }
 
   Future<void> _initializePreferences() async {
@@ -42,6 +116,15 @@ class _SettingsPageState extends State<SettingsPage> {
       _isDark = isDarkModeEnabled;
       _saveThemeValue(isDarkModeEnabled);
     });
+  }
+
+  void userInfoInit() {
+    final userService = Provider.of<ProviderService>(context, listen: false);
+    //debugPrint(userService.user['userInfo'].toString());
+    currentFriendListPrivacy = privacyOptions.keys
+        .toList()[userService.user['userInfo']['frnd_list_vis']];
+    currentLinkedPrivacy = privacyOptions.keys
+        .toList()[userService.user['userInfo']['linked_acc_vis']];
   }
 
   @override
@@ -115,13 +198,100 @@ class _SettingsPageState extends State<SettingsPage> {
                   ],
                 ),
                 const Divider(),
-                const _SingleSection(
+                _SingleSection(
                   title: "Privacy & Security",
                   children: [
                     Padding(
+                      padding: const EdgeInsets.only(left: 5.0),
+                      child: _CustomListTile(
+                        title: "Privacy",
+                        leading:
+                            const Icon(Icons.admin_panel_settings_outlined),
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Padding(
+                                padding: EdgeInsets.fromLTRB(8.0, 0, 0, 2),
+                                child: Text("Friend List privacy"),
+                              ),
+                              CustomDropdown<String>(
+                                decoration: CustomDropdownDecoration(
+                                  closedFillColor: _isDark
+                                      ? const Color.fromARGB(255, 231, 231, 231)
+                                      : const Color.fromARGB(
+                                          255, 243, 232, 232),
+                                  expandedFillColor:
+                                      const Color.fromARGB(255, 255, 249, 222),
+                                  listItemStyle:
+                                      const TextStyle(color: Colors.black),
+                                  hintStyle:
+                                      const TextStyle(color: Colors.black),
+                                  headerStyle: TextStyle(
+                                      color: privacyOptions[
+                                          currentFriendListPrivacy]),
+                                ),
+                                hintText: 'Select Privacy mode',
+                                items: privacyOptions.keys.toList(),
+                                initialItem: currentFriendListPrivacy,
+                                excludeSelected: true,
+                                onChanged: (value) {
+                                  //debugPrint(value);
+                                  setState(() {
+                                    currentFriendListPrivacy = value;
+                                  });
+                                  updateFriendListVis();
+                                },
+                              ),
+                            ],
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Padding(
+                                padding: EdgeInsets.fromLTRB(8.0, 0, 0, 2),
+                                child: Text("Linked Account privacy"),
+                              ),
+                              CustomDropdown<String>(
+                                decoration: CustomDropdownDecoration(
+                                  closedFillColor: _isDark
+                                      ? const Color.fromARGB(255, 231, 231, 231)
+                                      : const Color.fromARGB(
+                                          255, 243, 232, 232),
+                                  expandedFillColor:
+                                      const Color.fromARGB(255, 255, 249, 222),
+                                  listItemStyle:
+                                      const TextStyle(color: Colors.black),
+                                  hintStyle:
+                                      const TextStyle(color: Colors.black),
+                                  headerStyle: TextStyle(
+                                      color:
+                                          privacyOptions[currentLinkedPrivacy]),
+                                ),
+                                hintText: 'Select Privacy mode',
+                                items: privacyOptions.keys.toList(),
+                                initialItem: currentLinkedPrivacy,
+                                excludeSelected: true,
+                                onChanged: (value) {
+                                  //debugPrint(value);
+                                  setState(() {
+                                    currentLinkedPrivacy = value;
+                                  });
+                                  updateLinkedAccVis();
+                                },
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          )
+                        ],
+                      ),
+                    ),
+                    const Padding(
                       padding: EdgeInsets.only(left: 5.0),
                       child: _CustomListTile(
-                          title: "Security Status",
+                          title: "Security",
                           leading: Icon(Icons.lock_outline_rounded),
                           children: [
                             ListTile(
@@ -132,7 +302,7 @@ class _SettingsPageState extends State<SettingsPage> {
                             )
                           ]),
                     ),
-                    Padding(
+                    const Padding(
                       padding: EdgeInsets.only(left: 5.0),
                       child: _CustomListTile(
                           title: "Profile",
